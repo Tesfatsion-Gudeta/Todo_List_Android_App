84d2afc7e7098591f45f8340d6c365d537b984be
https://github.com/Tesfatsion-Gudeta <tesfatsiongudeta7@gmail.com>
2024-07-18 18:34:40 +0300

    layouts,components and functionalities

--- app/src/main/java/com/example/simpletodolistapp/MainActivity.java
+++ app/src/main/java/com/example/simpletodolistapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.simpletodolistapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton add, delete;
+    private LinearLayout layout;
+    private TextView task;
+    private EditText taskName;
+    AlertDialog alertDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,61 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dailog, null);
+        taskName = view.findViewById(R.id.tasknames);
+
+        builder.setView(view);
+
+        builder.setTitle("ADD A TASK");
+        builder.setPositiveButton("save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                addCard(taskName.getText().toString());
+            }
+        });
+
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        alertDialog = builder.create();
+
+
+        add = findViewById(R.id.floatingActionButton);
+        layout = findViewById(R.id.linearContainer);
+        task = findViewById(R.id.textView);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.show();
+            }
+        });
+
+    }
+
+    private void addCard(String taskName) {
+
+        View view = getLayoutInflater().inflate(R.layout.tasks, null);
+
+        delete = view.findViewById(R.id.floatingActionButton3);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.removeView(view);
+            }
+        });
+
+        layout.addView(view);
+
+        task = view.findViewById(R.id.textView2);
+        task.setText(taskName);
+
+
     }
 }
